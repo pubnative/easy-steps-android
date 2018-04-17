f20f65f5476df11484addf73eb7820a163ecd6ca
eros902002@googlemail.com <eros902002@googlemail.com>
2018-04-17 11:28:05 +0200

    Disable autorefresh on MoPub ads

--- app/src/main/java/net/pubnative/easysteps/ui/SettingsContainerFragment.java
+++ app/src/main/java/net/pubnative/easysteps/ui/SettingsContainerFragment.java
@@ -4,13 +4,25 @@ import android.app.Fragment;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import net.pubnative.easysteps.R;
+import com.mopub.mobileads.MoPubErrorCode;
+import com.mopub.mobileads.MoPubView;
 
-public class SettingsContainerFragment extends Fragment {
+import net.pubnative.easysteps.R;
+import net.pubnative.lite.sdk.api.MRectRequestManager;
+import net.pubnative.lite.sdk.api.RequestManager;
+import net.pubnative.lite.sdk.models.Ad;
+import net.pubnative.lite.sdk.utils.PrebidUtils;
+
+public class SettingsContainerFragment extends Fragment implements MoPubView.BannerAdListener {
+    private static final String TAG = SettingsContainerFragment.class.getSimpleName();
+
+    private MoPubView mMRectView;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
@@ -25,6 +37,71 @@ public class SettingsContainerFragment extends Fragment {
             transaction.commit();
         }
 
+        mMRectView = view.findViewById(R.id.mopub_mrect);
+        mMRectView.setAutorefreshEnabled(false);
+
         return view;
     }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        loadAd();
+    }
+
+    public void loadAd() {
+        RequestManager requestManager = new MRectRequestManager();
+        requestManager.setZoneId(getString(R.string.pnlite_mrect_zone_id));
+        requestManager.setRequestListener(new RequestManager.RequestListener() {
+            @Override
+            public void onRequestSuccess(Ad ad) {
+                if (getActivity() != null && isResumed()) {
+                    mMRectView.setAdUnitId(getString(R.string.mopub_mrect_ad_unit_id));
+                    mMRectView.setKeywords(PrebidUtils.getPrebidKeywords(ad, getString(R.string.pnlite_mrect_zone_id)));
+                    mMRectView.loadAd();
+                }
+            }
+
+            @Override
+            public void onRequestFail(Throwable throwable) {
+                if (getActivity() != null && isResumed()) {
+                    mMRectView.setAdUnitId(getString(R.string.mopub_mrect_ad_unit_id));
+                    mMRectView.loadAd();
+                }
+                Log.e(TAG, throwable.getMessage());
+            }
+        });
+        requestManager.requestAd();
+    }
+
+    @Override
+    public void onBannerLoaded(MoPubView banner) {
+        if (getActivity() != null && isResumed()) {
+            mMRectView.setVisibility(View.VISIBLE);
+        }
+    }
+
+    @Override
+    public void onBannerFailed(MoPubView banner, MoPubErrorCode errorCode) {
+        Log.e(TAG, errorCode.toString());
+    }
+
+    @Override
+    public void onBannerClicked(MoPubView banner) {
+        if (getActivity() != null && isResumed()) {
+            mMRectView.setVisibility(View.GONE);
+        }
+    }
+
+    @Override
+    public void onBannerExpanded(MoPubView banner) { }
+
+    @Override
+    public void onBannerCollapsed(MoPubView banner) { }
 }
